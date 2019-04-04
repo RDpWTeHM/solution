6443aee8327b223b852d4f90555fb604d4772a89
Joseph Lin <joseph.lin@aliyun.com>
2019-04-04 22:58:38 +0800

    [update] due to django-celery need setdefault-> django setting I have no idea about how to use settings in app.py So, separate the settings to a single file > settings.py

--- web/backend/celery/rabbitmq/django/addcalculate/add.py
+++ web/backend/celery/rabbitmq/django/addcalculate/add.py
@@ -8,42 +8,11 @@ Usage:
 
 import sys
 import os
-from django.conf import settings
+
 from django.urls import path
 
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import render
-
-
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
-
-BASE_DIR = os.path.dirname(__file__)
-
-
-settings.configure(
-    DEBUG=True,
-    SECRET_KEY='thisisthesecretkey',
-    ALLOWED_HOSTS=ALLOWED_HOSTS,
-    ROOT_URLCONF=__name__,
-    MIDDLEWARE_CLASSES=(
-        'django.middleware.common.CommonMiddleware',
-        'django.middleware.csrfViewMiddleware',
-        'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    ),
-    INSTALLED_APPS=(
-        'django.contrib.staticfiles',
-    ),
-    TEMPLATES=(
-        {
-            'BACKEND': 'django.template.backends.django.DjangoTemplates',
-            'DIRS': (os.path.join(BASE_DIR, 'templates'), )
-        },
-    ),
-    STATICFILES_DIRS=(
-        os.path.join(BASE_DIR, 'static'),
-    ),
-    STATIC_URL='/static/',
-)
 
 
 def index(request):
@@ -56,6 +25,8 @@ urlpatterns = (
 
 
 if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
---
+++ web/backend/celery/rabbitmq/django/addcalculate/settings.py
@@ -0,0 +1,99 @@
+# settings.py
+
+import os
+
+# import djcelery
+
+
+# djcelery.setup_loader()  # 加载 djcelery
+# BROKER_URL = 'pyamqp://guest@localhost//'
+# BROKER_POOL_LIMIT = 0
+# CELERY_RESULT_BACKEND = 'djcelery.backends.database:DatabaseBackend'
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+
+BASE_DIR = os.path.dirname(__file__)
+
+DEBUG = True
+
+SECRET_KEY = 'thisisthesecretkey'
+
+ROOT_URLCONF = 'add'  # addcalculate/add.py
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # 'djcelery',
+)
+
+TEMPLATES = (
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': (os.path.join(BASE_DIR, 'templates'), ),
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+)
+
+# Password validation
+# https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_URL = '/static/'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'Asia/Shanghai'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
