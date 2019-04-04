07ab80eb2dd5b104edb85d13e958ca6df42066d5
testfwupgradestress <na@na.com>
2019-04-04 16:34:31 +0800

    [demo] the hello world demo of lightweight django project

---
+++ web/backend/django/lightweight/hello.py
@@ -0,0 +1,41 @@
+# hello.py
+
+"""
+Usage:
+    $ python hello.py manage.py
+
+"""
+
+import sys
+import os
+from django.conf import settings
+from django.urls import path
+
+from django.http import HttpResponse
+
+
+settings.configure(
+    DEBUG=True,
+    SECRET_KEY='thisisthesecretkey',
+    ROOT_URLCONF=__name__,
+    MIDDLEWARE_CLASSES=(
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrfViewMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ),
+)
+
+
+def index(request):
+    return HttpResponse("<h1>Hello World</h1>")
+
+
+urlpatterns = (
+    path("", index, name="index"),
+)
+
+
+if __name__ == '__main__':
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
